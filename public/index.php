addba66d30fd7d32ee613ab22bc0c7d116be9ead
Alejandro Balladares <ale_12_river@hotmail.com>
2026-09-17 20:10:25 -0300

    Agrego las rutas relacionadas al login

--- public/index.php
+++ public/index.php
@@ -2,12 +2,24 @@
 
 require_once __DIR__ . '/../includes/app.php';
 
+use Controllers\loginController;
 use MVC\Router;
 
 $router = new Router();
 
+$router->get('/', [loginController::class, 'login']);
+$router->post('/', [loginController::class, 'login']);
+$router->get('/logout', [loginController::class, 'logout']);
 
+//recuperar password
+$router->get('/olvide', [loginController::class, 'olvide']);
+$router->post('/olvide', [loginController::class, 'olvide']);
+$router->get('/recuperar', [loginController::class, 'recuperar']);
+$router->post('/recuperar', [loginController::class, 'recuperar']);
 
+//crear cuenta
+$router->get('/crear-cuenta', [loginController::class, 'crear']);
+$router->post('/crear-cuenta', [loginController::class, 'crear']);
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
