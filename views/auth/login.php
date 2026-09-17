f3a8e63f9993750593d610726c086b3d8608e05b
Alejandro Balladares <ale_12_river@hotmail.com>
2026-09-17 20:09:45 -0300

    Agrego la vista del login

---
+++ views/auth/login.php
@@ -0,0 +1,21 @@
+<h1 class="nombre-pagina">Login</h1>
+<p class="descripcion-pagina">Inicia sesion con tus datos</p>
+
+<fomr class="formulario" method="POST" action="/">
+    <div class="campo">
+        <label for="email">Email</label>
+        <input type="email" id="email" placeholder="Tu email" name="email">
+    </div>
+
+    <div class="campo">
+        <label for="password">Password</label>
+        <input type="password" id="password" placeholder="Tu password" name="password">
+    </div>
+
+    <input type="submit" class="boton" value="Iniciar Sesion">
+</fomr>
+
+<div class="acciones">
+    <a href="/crear-cuenta">¿Aún no tienes cuenta? Crea una</a>
+    <a href="/olvide">¿Has olvidado tu contraseña? Click aquí</a>
+</div>
