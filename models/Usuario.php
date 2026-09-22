294f0bde1d39f656a82bc6e06dc0e20b1ec9785a
Alejandro Balladares <ale_12_river@hotmail.com>
2026-09-22 20:16:46 -0300

    Agrego metodos al usuario para validar, hashear y generar tokens

--- models/Usuario.php
+++ models/Usuario.php
@@ -25,28 +25,53 @@ class Usuario extends ActiveRecord {
         $this->email = $args['email'] ?? '';
         $this->telefono = $args['telefono'] ?? '';
         $this->password = $args['password'] ?? '';
-        $this->admin = $args['admin'] ?? 0;
-        $this->confirmado = $args['confirmado'] ?? null;
+        $this->admin = $args['admin'] ?? '0';
+        $this->confirmado = $args['confirmado'] ?? '0';
         $this->token = $args['token'] ?? '';
     }
 
     //mensaje de validacion para la creacion de la cuenta
     public function validarNuevaCuenta(){
         if(!$this->nombre){
-            self::$alertas['error'][] = 'El nombre del cliente es obligatorio';
+            self::$alertas['error'][] = 'El nombre es obligatorio';
         }
         if(!$this->apellido){
-            self::$alertas['error'][] = 'El apellido del cliente es obligatorio';
+            self::$alertas['error'][] = 'El apellido es obligatorio';
         }
         if(!$this->telefono){
-            self::$alertas['error'][] = 'El telefono del cliente es obligatorio';
+            self::$alertas['error'][] = 'El telefono es obligatorio';
         }
         if(!$this->email){
-            self::$alertas['error'][] = 'El email del cliente es obligatorio';
+            self::$alertas['error'][] = 'El email es obligatorio';
         }
         if(!$this->password){
-            self::$alertas['error'][] = 'El password del cliente es obligatorio';
+            self::$alertas['error'][] = 'El password es obligatorio';
         }
+
+        if(strlen($this->password) < 6){
+            self::$alertas['error'][] = 'El password debe contener al menos 6 caracteres';
+        }
+
         return self::$alertas;
+    }
+
+    //Revisa si ya está registrado
+    public function existeUsuario(){
+        $query = "SELECT * FROM " . self::$tabla . " WHERE email = '" . $this->email . "' LIMIT 1" ;
+        $resultado = self::$db->query($query);
+
+        if($resultado->num_rows){
+            self::$alertas['error'][] = 'El usuario ya está registrado';
+        }
+        //debuguear($resultado);
+        return $resultado;
+    }
+
+    public function hashPassword(){
+        $this->password = password_hash($this->password, PASSWORD_BCRYPT);
+    }
+
+    public function generarToken(){
+        $this->token = uniqid();
     }
 }
