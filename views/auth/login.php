bef83c2a9be02a470133b2acc936205b636814fc
Alejandro Balladares <ale_12_river@hotmail.com>
2026-09-25 19:38:24 -0300

    Agrego las alertas al inicio de sesion

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,7 +1,9 @@
 <h1 class="nombre-pagina">Login</h1>
 <p class="descripcion-pagina">Inicia sesion con tus datos</p>
 
-<fomr class="formulario" method="POST" action="/">
+<?php include_once __DIR__ . "/../templates/alertas.php"; ?>
+
+<form class="formulario" method="POST" action="/">
     <div class="campo">
         <label for="email">Email</label>
         <input type="email" id="email" placeholder="Tu email" name="email">
@@ -13,7 +15,7 @@
     </div>
 
     <input type="submit" class="boton" value="Iniciar Sesion">
-</fomr>
+</form>
 
 <div class="acciones">
     <a href="/crear-cuenta">¿Aún no tienes cuenta? Crea una</a>
